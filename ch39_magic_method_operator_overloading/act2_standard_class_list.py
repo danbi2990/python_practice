d703f4b85dca6f6543363c4f9ec1e1ef4e6a61e0
danbi2990 <danbi2990@gmail.com>
2017-01-25 22:38:01 +0900

    from campus

---
+++ ch39_magic_method_operator_overloading/act2_standard_class_list.py
@@ -0,0 +1,14 @@
+class Plist(list):
+
+    def __init__(self, l):
+        list.__init__(self, l)
+
+    def push(self, item):
+        self.append(item)
+
+
+if __name__ == "__main__":
+    x = Plist([3,4])
+    x.push(47)
+    print(x)
+    
